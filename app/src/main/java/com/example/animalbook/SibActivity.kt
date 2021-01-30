6b000be666f6536bb935cdc788676ab2ddcd2c18
dai03 <d.taniguchi0803@gmail.com>
2021-01-30 19:04:53 +0900

    work_20210130_2_activity_sib_addFragment

--- app/src/main/java/com/example/animalbook/SibActivity.kt
+++ app/src/main/java/com/example/animalbook/SibActivity.kt
@@ -2,10 +2,51 @@ package com.example.animalbook
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.animalbook.databinding.ActivitySibBinding
 
 class SibActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySibBinding
+    private lateinit var title: TitleFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sib)
+        binding = ActivitySibBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lionButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, LionFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.hippoButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, HippoFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        binding.giraffeButton.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.container, GiraffeFragment())
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        title = TitleFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.titleFrame, title)
+            commit()
+        }
     }
+
+    override fun onResume(){
+        super.onResume()
+        title.setTitle("サブ画面")
+    }
+
+
 }
